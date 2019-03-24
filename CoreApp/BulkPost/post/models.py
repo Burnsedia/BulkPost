fcab61a5e9a8f42b73bd7a4fd121843169029985
Burnsedia <bburnsed1995@gmail.com>
2019-03-23 22:22:29 -0400

    added some datamodels, still needs work

--- CoreApp/BulkPost/post/models.py
+++ CoreApp/BulkPost/post/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class post(models.Model):
+    def __init__(self):
+       user_name =  
--- CoreApp/BulkPost/user/models.py
+++ CoreApp/BulkPost/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class user(models.Models.):
+    def __init__(self):
+        pass
+
+    
