e0ec7b733d95e4caefcadd5415adf3d0c1a66066
Burnsedia <bburnsed1995@gmail.com>
2019-03-24 18:00:34 -0400

    added more code to models

--- CoreApp/BulkPost/post/models.py
+++ CoreApp/BulkPost/post/models.py
@@ -3,4 +3,11 @@ from django.db import models
 # Create your models here.
 class post(models.Model):
     def __init__(self):
-       user_name =  
+       pass
+
+    def get_user_input(self):
+        pass
+
+    def render_post(self):
+        pass
+     
--- CoreApp/BulkPost/user/models.py
+++ CoreApp/BulkPost/user/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class user(models.Models.):
+class user(models.Models):
     def __init__(self):
         pass
+    def getset_user_name():
+        pass
+    def user_pormissions(self):
+        # lockup django admins
+        pass
+     
 
     
