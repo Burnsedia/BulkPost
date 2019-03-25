22b8ebba13bb1847eb7df79a00c0c9883f9ad5e0
Burnsedia <bburnsed1995@gmail.com>
2019-03-25 13:48:41 -0400

    fenished user and post models

--- CoreApp/BulkPost/post/models.py
+++ CoreApp/BulkPost/post/models.py
@@ -1,13 +1,11 @@
 from django.db import models
-
 # Create your models here.
 class post(models.Model):
-    def __init__(self):
-       pass
+   # add post class properties here
+   post_title =  models.CharField(max_length = 20)
+   post_content = models.TextField():
+   post_date = modoels.DateTimeField(default= timezone.now)
+   def __str__(self):
+       return '<Name:{}, ID:{}'.format(self.name,self.id)
 
-    def get_user_input(self):
-        pass
 
-    def render_post(self):
-        pass
-     
--- CoreApp/BulkPost/user/models.py
+++ CoreApp/BulkPost/user/models.py
@@ -1,15 +1,10 @@
-from django.db import models
+fauto-completionrom django.db import models
 
 # Create your models here.
 
 class user(models.Models):
-    def __init__(self):
-        pass
-    def getset_user_name():
-        pass
-    def user_pormissions(self):
-        # lockup django admins
-        pass
-     
+    user_name = models.CharField(max_length = 20)
+    user_password = models.CharField(max_length = 20)
+    # Add user paid tier or use browser miner for monetizing the app  
 
     
