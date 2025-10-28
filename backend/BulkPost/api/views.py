94a6a927cfa64de2b20a316a35d0efdebcd01896
Burnsedia <bburnsed1995@gmail.com>
2025-10-27 20:36:08 -0400

    coding without ai is so much fun

--- backend/BulkPost/api/views.py
+++ backend/BulkPost/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from .models import SystemPrompt, Prompt, Post
+from .serializers import Sy
 
-# Create your views here.
+
+
+# Create your views here
+class SystemPromptView(ModelViewSet):
+    pass
+
+class PromptView(ModelViewSet):
+    pass
+
+class PostView(ReadOnlyModelViewSet):
+    pass
