1dd7b5789188ead6a04fb9d19eba71add098e586
Burnsedia <bburnsed1995@gmail.com>
2025-10-27 20:42:28 -0400

    all the views are set up

--- backend/BulkPost/api/views.py
+++ backend/BulkPost/api/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from .models import SystemPrompt, Prompt, Post
-from .serializers import Sy
-
-
+from .serializers import SystemPromptSerializer, PromptSerializer, PostSerializer
 
 # Create your views here
 class SystemPromptView(ModelViewSet):
-    pass
+    serializer_class = SystemPromptSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return SystemPrompt.objects.filter(user=self.request.user)
 
 class PromptView(ModelViewSet):
-    pass
+    serializer_class = PromptSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Prompt.objects.filter(user=self.request.user)
+
 
 class PostView(ReadOnlyModelViewSet):
-    pass
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Post.objects.filter(user=self.request.user)
+
