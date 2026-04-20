035ab23463614a02a241d1a164e60a2ed35e9d7c
Burnsedia <bburnsed1995@gmail.com>
2026-04-20 15:03:54 -0400

    chore(backend): update model import and compiled artifacts

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.db import models
-from django.utils import timezone
-
+from django.utils import timezon
 
 class Category(models.TextChoices):
     VALUE = "value", "Value"
