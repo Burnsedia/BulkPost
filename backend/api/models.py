38f0034ae63756a371609c95d2f5e1731decefc7
Burnsedia <bburnsed1995@gmail.com>
2026-04-23 14:03:15 -0400

    I did commit this yesterday

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,8 +2,78 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
 class Category(models.Model):
     title = models.CharField()
     slug = models.SlugField()
     description = models.TextField()
-    created_at = models.Date
+    created_at = models.DateTimeField(auto_now=True)
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="children"
+    )
+
+class Post(models.Model):
+    title = models.CharField()
+    slug = models.SlugField()
+    content = models.TextField()
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="PostCategory"
+    )
+)
+
+class PostStatus(models.Model):
+    title = models.CharField()
+
+class Schedlutle(models.Model):
+    schedulted_time = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey(
+        Post, 
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="SchedlutledPost"
+    )
+
+class TwitterPost(models.Model):
+    twitterID = models.CharField(max_length=50)
+    post = models.ForeignKey(
+        Post, 
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="SchedlutledPost"
+    )
+
+class TwitterPost(models.Model):
+    linkedinID = models.CharField(max_length=50)
+    post = models.ForeignKey(
+        Post, 
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="SchedlutledPost"
+    )
+
+class ThreadsPostPost(models.Model):
+    threadsID = models.CharField(max_length=50)
+    post = models.ForeignKey(
+        Post, 
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="SchedlutledPost"
+    )
+
+#TODO: create text choice models or a plateform model
+class Contact(models.Models):
+    username = models.CharField(max_length=50)
+    platform = models.CharField(max_length=50)
+    bio = models.TextField()
